2af12bd6095a435b33c2f9c81233dc6c6dc331d1
Martijn de Vos <martijn.devos@epfl.ch>
2023-01-19 16:07:34 +0100

    Initial commit

---
+++ scripts/r/plot_accuracies_comparison.r
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(dplyr)
+
+dat <- read.csv("../../data/accuracies_comparison.csv")
+dat$peers <- as.factor(dat$peers)
+
+dat <- dat %>%
+  group_by(dataset, algorithm, peers) %>%
+  summarise(acc = mean(accuracy), acc_sd = sd(accuracy))
+
+print(dat)
+
+p <- ggplot(dat, aes(x=peers, y=acc, group=algorithm, color=algorithm)) +
+    geom_line() +
+    geom_point(aes(shape=algorithm)) +
+    geom_errorbar(aes(ymin=acc-acc_sd, ymax=acc+acc_sd)) +
+    xlab("Num. clients (c)") +
+    ylab("Test Accuracy") +
+    theme_bw() +
+    theme(legend.position="top", legend.box.background = element_rect(colour = "black"), legend.title=element_blank())
+
+ggsave("../../plots/accuracies_comparison.pdf", p, width=5, height=2.7)
